3caaa0f838815601bd30fac387ec3c594bf48a29
Qrow <nachoesposito@gmail.com>
2024-04-14 01:21:37 +0200

    Mensaje de Bienvenida

--- newmembers.py
+++ newmembers.py
@@ -6,5 +6,5 @@ import threading
 async def new_members(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if update.message.new_chat_members:
                 chat_id = update.message.chat_id
-                await context.bot.send_message(chat_id, "Bienvenido á canle de Sysarmy Galicia, esperamos que o pases moi ben nesta comunida! \n\n Bienvenido al canal de Sysarmy Galicia, esperamos que lo pases muy bien en esta comunida!\n\n Welcome to the Sysarmy Galicia channel, we hope you have a great time within this community!", message_thread_id=1669)
+                await context.bot.send_message(chat_id, f"A wild {update.message.new_chat_members[0].username} appears", message_thread_id=1669)
       
