b1a6fbef38dcf7c48ad9e8e3c000942756faddea
Qrow <nachoesposito@gmail.com>
2024-04-15 22:51:38 +0200

    Meetup updated

--- newmembers.py
+++ newmembers.py
@@ -6,5 +6,5 @@ import threading
 async def new_members(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if update.message.new_chat_members:
                 chat_id = update.message.chat_id
-                await context.bot.send_message(chat_id, f"A wild {update.message.new_chat_members[0].username} appears", message_thread_id=1669)
+                await context.bot.send_message(chat_id, f"A wild @{update.message.new_chat_members[0].username} appears", message_thread_id=1669)
       
