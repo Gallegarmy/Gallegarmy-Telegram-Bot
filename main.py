09839c533e555e64e566e50f85bbbcc5c4aca746
madtyn <madtyn@gmail.com>
2024-07-31 17:02:33 +0200

    Refactor del agregado de handlers a la application

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from telegram.ext import Updater, CommandHandler, ApplicationBuilder, MessageHandler, filters
+from telegram.ext import CommandHandler, ApplicationBuilder, MessageHandler, filters
+
 from start import start
 from newmembers import new_members
 from cerveza import cerveza, cervezahora, cervezalugar, cervezamapa, cervezaasistencia,cervezalink
@@ -8,86 +9,53 @@ from fiestas import festivos
 from karma import kup, kdown, kshow, klist
 from dinner import startDinner, dinnerOrder, roundOrder, changePrice, endDinner, beerTaker, changeMenu, removeItemOrder
 import tracemalloc
+import os
 tracemalloc.start()
 
 
+def get_bot_token():
+    return os.environ['BOT_TOKEN']
+    # return '6836403587:AAFaoLBovQo-Sd69RcgUk_uFTkZctRcyqZY'
+
+
 def main():
-    
     # Create an updater object with your bot's token
-    application = ApplicationBuilder().token('6836403587:AAFaoLBovQo-Sd69RcgUk_uFTkZctRcyqZY').build()
+    application = ApplicationBuilder().token(get_bot_token()).build()
 
-
-    # Add a handler for the /start command
-    application.add_handler(CommandHandler("start", start))
-
-    # Add a handler for the /cañas command
-    application.add_handler(CommandHandler("cerveza", cerveza))
-
-    # Add a handler for changing the cañas time
-    application.add_handler(CommandHandler("cervezahora", cervezahora))
-
-    # Add a handler for changing the cañas place
-    application.add_handler(CommandHandler("cervezalugar", cervezalugar))
-
-    # Add a handler for changing the cañas map
-    application.add_handler(CommandHandler("cervezamapa", cervezamapa))
-
-    # Add a handler for changing the cañas assistance
-    application.add_handler(CommandHandler("cervezamapa", cervezaasistencia))
-
-    # Add a handler for changing the cañas link
-    application.add_handler(CommandHandler("cervezalink", cervezalink))
-
-     # Add a handler for the /cañas command
-    application.add_handler(CommandHandler("help", help))
-
-    # Add a handler for the /festivos command
-    application.add_handler(CommandHandler("festivos", festivos))
-
-    #Add a handler for the new member entering the chat
+    # Add a handler for the new member entering the chat
     application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, new_members))
 
-    #Add a handler for adding karma to user
-    application.add_handler(CommandHandler("kup", kup))
+    commands = {
+        'beer': beerTaker,
+        'cerveza': cerveza,
+        'cervezahora': cervezahora,
+        'cervezaasistencia': cervezaasistencia,
+        'cervezalugar': cervezalugar,
+        'cervezamapa': cervezamapa,
+        'cervezalink': cervezalink,
+        'enddinner': endDinner,
+        'festivos': festivos,
+        'help': help,
+        'kup': kup,
+        'kdown': kdown,
+        'klist': klist,
+        'kshow': kshow,
+        "menuchange": changeMenu,
+        'order': dinnerOrder,
+        "orderchange": removeItemOrder,
+        'pineapple': pinacolada,
+        'pricechange': changePrice,
+        'roundOrder': roundOrder,
+        'start': start,
+        'startdinner': startDinner,
+    }
 
-    #Add a handler for removing karma to user
-    application.add_handler(CommandHandler("kdown", kdown))
-
-    #Add a handler for showing karma of user
-    application.add_handler(CommandHandler("kshow", kshow))
-
-    #Add a handler for showing karma top
-    application.add_handler(CommandHandler("klist", klist))
-
-    #Add a handler for the Pina command
-    application.add_handler(CommandHandler("pineapple", pinacolada))
-
-    #Add a handler for Starting Dinner
-    application.add_handler(CommandHandler("startdinner", startDinner))
-
-    #Add a handler for Ordering Dinner
-    application.add_handler(CommandHandler("order", dinnerOrder))
-
-    #Add a handler for Closing Order Round
-    application.add_handler(CommandHandler("roundorder", roundOrder))
-
-    #Add a handler for Changing Item Price
-    application.add_handler(CommandHandler("pricechange", changePrice))
-
-    #Add a handler for Ending Dinner
-    application.add_handler(CommandHandler("enddinner", endDinner))
-
-    #Add a handler for Drinking Beer
-    application.add_handler(CommandHandler("beer", beerTaker))
-
-    #Add a handler for Adding / Modifying menu
-    application.add_handler(CommandHandler("menuchange", changeMenu))
-
-    #Add a handler for Deleting an x amount of item from the order
-    application.add_handler(CommandHandler("orderchange", removeItemOrder))
+    for comm_string, funct in commands.items():
+        application.add_handler(CommandHandler(comm_string, funct))
 
     # Start the Bot
     application.run_polling()
+
 
 if __name__ == '__main__':
     main()
