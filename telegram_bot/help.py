dc78ed84effe5d82b427340a2f3c4fd21a19ec2f
Néstor <nestor@nstlopez.com>
2024-08-24 13:39:50 +0200

    feat: cleanup help

--- telegram_bot/help.py
+++ telegram_bot/help.py
@@ -1,12 +1,20 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ContextTypes
 
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    thread_id = update.message.message_thread_id
-    print(thread_id)
+    if update.message is not None:
+        thread_id = update.message.message_thread_id
+    else:
+        thread_id = None
+
     admins = [line.strip() for line in open("admins.txt")]
-    if str(update.message.from_user.username) in admins:
+
+    if (
+        update.message is not None
+        and update.message.from_user is not None
+        and str(update.message.from_user.username) in admins
+    ):
         help = ""
         with open("helpadmin.txt", "r", encoding="utf-8") as help_file:
             for line in help_file:
@@ -22,8 +30,10 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
             for line in help_file:
                 help = help + line
                 help = help + "\n"
-
-        await context.bot.send_message(
-            chat_id=update.message.chat_id, text=f"{help}", message_thread_id=thread_id
-        )
+        if update.message is not None:
+            await context.bot.send_message(
+                chat_id=update.message.chat_id,
+                text=f"{help}",
+                message_thread_id=thread_id,
+            )
         return
