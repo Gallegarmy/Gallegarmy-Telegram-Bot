c11d0a6cd26fb1ad392fc01fc981f159e13c267f
Qrow <nachoesposito@gmail.com>
2024-12-31 16:05:14 +0100

    fix: Added the eventbrite url in events

--- telegram_bot/cerveza.py
+++ telegram_bot/cerveza.py
@@ -46,7 +46,7 @@ async def events(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 f"Próximo evento de Sysarmy Galicia:\n\n"
                 f"{event['name'].get('text')}\n\n"
                 f"Fecha y Hora: {event_time}\n\n"
-                f"Meetup: '{event['url']}'\n\n"
+                f"Meetup: {event['url']}\n\n"
             )
             if update.effective_chat:
                 await context.bot.send_message(
