eafdd48e1a0cd001c7a3f58e603c2a5743f1c5bf
Ignacio Espósito <nachoesposito@gmail.com>
2026-06-05 17:21:09 +0200

    feat: added the option to filter event through city

--- telegram_bot/events/check_events_city.py
+++ telegram_bot/events/check_events_city.py
@@ -1,13 +1,25 @@
 from datetime import datetime 
 
 
-def check_event(events):
+def check_event_city(events,city: str):
 
     if len(events) <= 1:
         return events[0]
 
-    current_event = events[0]
-    next_event = events [1]
+    for i,event in enumerate(events):
+        if city == 'coruna':
+            if 'admin' in event["name"]["text"].strip().lower():
+                return check_repeated_event(event,events[i+1])
+        elif city == 'vigo':
+            if 'olivarmy' in event["name"]["text"].strip().lower():
+                return check_repeated_event(event,events[i+1])
+        i+=1
+
+        return "No event"
+
+
+
+def check_repeated_event(current_event, next_event):
     current_event_name = current_event["name"]["text"].strip().lower()
     next_event_name = next_event["name"]["text"].strip().lower()
     current_event_month = datetime.fromisoformat(current_event["start"]["local"]).month
